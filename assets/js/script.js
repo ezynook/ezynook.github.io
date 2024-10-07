59a509d6b32ce54fd1a5ed59843c4c3939164630
Pasit DE <ezynook@gmail.com>
2024-10-07 17:42:28 +0700

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Pasit Yodsoi";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Engineer", "Backend Development"],
+    strings: ["Data Engineer", "Backend Developer", "Full stack Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
